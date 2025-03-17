40a305eeb1c6bd59471b2ca708cba8ed76ff8d40
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-03-17 23:21:33 +0100

    refactor(test): use buffer_contains function

--- tests/unit/init_spec.lua
+++ tests/unit/init_spec.lua
@@ -2,7 +2,15 @@ require("luassert")
 local ui = require("ascii-ui")
 local Box = require("ascii-ui.components.box")
 
-local eq = assert.are.same
+---@param bufnr integer
+---@param pattern string
+---@return boolean
+local function buffer_contains(bufnr, pattern)
+	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+	local content_str = vim.iter(lines):join("")
+
+	return string.find(content_str, pattern) ~= nil
+end
 
 describe("ascii-ui", function()
 	it("ui.render() should render component", function()
@@ -13,10 +21,6 @@ describe("ascii-ui", function()
 		local bufnr = ui.render(component)
 
 		assert(bufnr)
-		-- read the buf content
-		local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-		local lines_str = vim.iter(lines):join("")
-
-		assert(string.find(lines_str, expected_msg))
+		assert(buffer_contains(bufnr, expected_msg))
 	end)
 end)
