be1830d94ea0b4e7f246502c60ff1f4713dab145
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-03-18 04:10:41 +0100

    fix: recognize child component methods

--- tests/unit/components/component_spec.lua
+++ tests/unit/components/component_spec.lua
@@ -3,17 +3,33 @@ local eq = assert.are.same
 
 local Component = require("ascii-ui.components.component")
 
+local create_dummy_component = function()
+	local DummyComponent = {
+		name = "DummyComponent",
+	}
+	function DummyComponent:new()
+		return Component:extend(self)
+	end
+
+	function DummyComponent:is_true()
+		return true
+	end
+
+	return DummyComponent:new()
+end
+
 describe("Component", function()
 	it("should be extensible", function()
-		local component = Component:extend({ name = "DummyComponent" })
+		local component = create_dummy_component()
 		eq("DummyComponent", component.name)
 		eq("BaseComponent", component.__name)
+		assert(component:is_true())
 	end)
 
 	it("should subscribe to functions and run them on state changes", function()
 		local interactions_count = 0
 
-		local component = Component:extend({ name = "DummyComponent" })
+		local component = create_dummy_component()
 
 		component:subscribe(function()
 			interactions_count = interactions_count + 1
