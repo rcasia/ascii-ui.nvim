b2175f793051603127ed1a7d669ecfe4ff42c864
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-03-19 02:21:33 +0100

    doc: update docstring for checkbox

--- lua/ascii-ui/components/checkbox.lua
+++ lua/ascii-ui/components/checkbox.lua
@@ -3,6 +3,8 @@ local BufferLine = require("ascii-ui.buffer.bufferline")
 local Element = require("ascii-ui.buffer.element")
 
 ---@class ascii-ui.Checkbox : ascii-ui.Component
+---@field checked boolean
+---@field label string
 local Checkbox = {}
 
 ---@param opts? { checked?: boolean, label?: string }
@@ -11,7 +13,6 @@ function Checkbox:new(opts)
 	opts = opts or {}
 	opts.checked = opts.checked or false
 	opts.label = opts.label or ""
-	opts.type = "checkbox"
 	return Component:extend(self, opts)
 end
 
@@ -23,7 +24,7 @@ function Checkbox:is_checked()
 	return self.checked
 end
 
----@return ascii-ui.BufferLines[]
+---@return ascii-ui.BufferLine[]
 function Checkbox:render()
 	return { BufferLine:new(Element:new(("[%s] %s"):format(self.checked and "x" or " ", self.label))) }
 end
