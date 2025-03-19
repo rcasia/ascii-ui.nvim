dd9d05c26b7e9fcdaf577b7d1dcc4b17e3c1fdcb
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-03-19 01:22:55 +0100

    feat: switch selects next option

--- lua/ascii-ui/components/switch.lua
+++ lua/ascii-ui/components/switch.lua
@@ -1,11 +1,30 @@
 local Component = require("ascii-ui.components.component")
 
+---@alias ascii-ui.SwitchOpts { options: string[] }
+
 ---@class ascii-ui.Switch : ascii-ui.Component
+---@field options string[]
+---@field _index_selected integer
 local Switch = {}
 
----@return ascii-ui.TextInput
-function Switch:new()
-	return Component:extend(self, {})
+---@param opts ascii-ui.SwitchOpts
+---@return ascii-ui.Switch
+function Switch:new(opts)
+	local state = {
+		options = opts.options,
+		_index_selected = 1,
+	}
+	return Component:extend(self, state)
+end
+
+---@return string selected_option
+function Switch:select_next()
+	if #self.options == self._index_selected then
+		self._index_selected = 1
+	else
+		self._index_selected = self._index_selected + 1
+	end
+	return self.options[self._index_selected]
 end
 
 return Switch
--- tests/unit/components/switch_spec.lua
+++ tests/unit/components/switch_spec.lua
@@ -4,7 +4,12 @@ local eq = assert.are.same
 local Switch = require("ascii-ui.components.switch")
 
 describe("Switch", function()
-	it("should create", function()
-		local switch = Switch:new()
+	it("should should be able to switch options", function()
+		local options = { "on", "off" }
+		local switch = Switch:new({ options = options })
+
+		eq("off", switch:select_next())
+
+		eq("on", switch:select_next())
 	end)
 end)
