3faf937da7b9040e4df9a8ca5091bfdfb819469a
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-04-02 15:58:58 +0200

    refactor: remove print statement

--- lua/ascii-ui/init.lua
+++ lua/ascii-ui/init.lua
@@ -43,9 +43,7 @@ function M.mount(component)
 			:interact({ buffer_id = bufnr, position = position, interaction_type = interaction_type.SELECT })
 	end, { buffer = window.bufnr, noremap = true, silent = true })
 
-	vim.on_key(function(key, typed)
-		print(vim.inspect({ key = key, typed = typed }))
-
+	vim.on_key(function(key, _)
 		local bufnr = vim.api.nvim_get_current_buf()
 		local cursor = vim.api.nvim_win_get_cursor(0)
 		local position = { line = cursor[1], col = cursor[2] }
