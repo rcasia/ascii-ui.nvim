41ad7f0d8ff34b1ac0b3f2b05501d5dc7e15a3bb
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-04-15 23:49:26 +0200

    feat(button): add on_press function

--- lua/ascii-ui/components/button.lua
+++ lua/ascii-ui/components/button.lua
@@ -2,11 +2,13 @@ local Component = require("ascii-ui.components.component")
 local Bufferline = require("ascii-ui.buffer.bufferline")
 local Element = require("ascii-ui.buffer.element")
 local highlights = require("ascii-ui.highlights")
+local interaction_type = require("ascii-ui.interaction_type")
 
---- @alias ascii-ui.ButtonComponent.Props { label: string }
+--- @alias ascii-ui.ButtonComponent.Props { label: string, on_press?: fun() }
 
 ---@class ascii-ui.ButtonComponent : ascii-ui.Component
----@field label string
+--- @field label string
+--- @field private on_press function
 local Button = {
 	__name = "ButtonComponent",
 }
@@ -17,13 +19,23 @@ function Button:new(props)
 	props = props or {}
 	local state = {
 		label = props.label or "",
+		on_press = props.on_press,
 	}
 	return Component:extend(self, state)
 end
 
 ---@return ascii-ui.BufferLine[]
 function Button:render()
-	return { Bufferline:new(Element:new({ content = self.label, highlight = highlights.BUTTON, is_focusable = true })) }
+	return {
+		Bufferline:new(Element:new({
+			content = self.label,
+			highlight = highlights.BUTTON,
+			is_focusable = true,
+			interactions = {
+				[interaction_type.SELECT] = self.on_press,
+			},
+		})),
+	}
 end
 
 return Button
