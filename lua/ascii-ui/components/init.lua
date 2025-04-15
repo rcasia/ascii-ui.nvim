3ed2f5e00865dd9e369351a095f2955638550ca2
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-04-15 23:31:11 +0200

    feat(button): expose button component

--- lua/ascii-ui/components/init.lua
+++ lua/ascii-ui/components/init.lua
@@ -2,6 +2,7 @@ local Components = {
 	options = require("ascii-ui.components.options"),
 	slider = require("ascii-ui.components.slider"),
 	paragraph = require("ascii-ui.components.paragraph"),
+	button = require("ascii-ui.components.button"),
 }
 
 return Components
