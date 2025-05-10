dbefc771b015a54345139f399ba54e9a3d59516b
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-10 18:25:31 +0200

    doc: add example for conditional rendering

---
+++ examples/conditional-rendering.lua
@@ -0,0 +1,33 @@
+local ui = require("ascii-ui")
+local Paragraph = ui.components.Paragraph
+local Slider = ui.components.Slider
+local Layout = ui.layout
+local Button = ui.components.Button
+local useState = ui.hooks.useState
+local If = require("ascii-ui.components.if")
+
+--- @type ascii-ui.FunctionalComponent
+local function App()
+	local shouldShow, setShouldShow = useState(true)
+
+	return function()
+		return Layout(
+			If({
+				condition = function()
+					return shouldShow()
+				end,
+				child = Paragraph({ content = "this is my content" }),
+				fallback = Paragraph({ content = "hidden" }),
+			}),
+
+			Button({
+				label = "change",
+				on_press = function()
+					setShouldShow(not shouldShow())
+				end,
+			})
+		)()
+	end
+end
+
+ui.mount(App())
