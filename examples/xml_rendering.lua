277ce54d8110c7f18f8e7ff08cce9187e92d791a
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-11 19:15:12 +0200

    doc: add greater example for xml rendering

--- examples/xml_rendering.lua
+++ examples/xml_rendering.lua
@@ -1,13 +1,16 @@
 local ui = require("ascii-ui")
-local useState = ui.hooks.useState
 
 --- @type ascii-ui.FunctionalComponent
 local function App()
 	return function()
-		-- return ui.layout(ui.components.Slider())
 		return [[
 
-		 <Paragraph content="devs" />
+		<Layout>
+		 	<Paragraph content="Hello World" />
+			<Layout>
+				<Button label="Click me" />
+			</Layout>
+		</Layout>
 
 		]]
 	end
