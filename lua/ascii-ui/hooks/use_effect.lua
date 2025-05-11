41efade1ff8417e2fb25d556fe15b1991e82c739
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-11 20:25:24 +0200

    feat(useEffect): invokes function everytime observed values change

--- lua/ascii-ui/hooks/use_effect.lua
+++ lua/ascii-ui/hooks/use_effect.lua
@@ -1,5 +1,28 @@
-local useEffect = function(fn)
+local EventListener = require("ascii-ui.events")
+
+local useEffect = function(fn, observed_values)
 	fn()
+
+	if not observed_values then
+		return
+	end
+
+	local last_seen_values = vim.iter(observed_values)
+		:map(function(observed_value)
+			return observed_value()
+		end)
+		:totable()
+
+	EventListener:listen("state_change", function()
+		local current_seen_values = vim.iter(observed_values):map(function(observed_value)
+			return observed_value()
+		end)
+
+		local has_changes = not vim.deep_equal(last_seen_values, current_seen_values)
+		if has_changes then
+			fn()
+		end
+	end)
 end
 
 return useEffect
--- tests/unit/hooks/use_effect_spec.lua
+++ tests/unit/hooks/use_effect_spec.lua
@@ -3,6 +3,7 @@ pcall(require, "luacov")
 
 local eq = assert.are.same
 local useEffect = require("ascii-ui.hooks.use_effect")
+local useState = require("ascii-ui.hooks.use_state")
 
 describe("useEffect", function()
 	it("invokes function upon first call", function()
@@ -13,5 +14,18 @@ describe("useEffect", function()
 		useEffect(fn)
 
 		eq(1, fn_invocations)
+	end)
+
+	it("invokes function every time observed values change", function()
+		local value, setValue = useState("A")
+		local fn_invocations = 0
+		local fn = function()
+			fn_invocations = fn_invocations + 1
+		end
+
+		useEffect(fn, { value })
+
+		setValue("B")
+		eq(2, fn_invocations)
 	end)
 end)
