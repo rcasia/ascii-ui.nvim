d24c8e9575ecdfb75a445acc2d9a5cbc1e2038fa
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-17 12:04:19 +0200

    feat(for): add example
    #20

---
+++ examples/collection_rendering.lua
@@ -0,0 +1,34 @@
+local ui = require("ascii-ui")
+local Paragraph = ui.components.Paragraph
+local Layout = ui.layout
+local Button = ui.components.Button
+local useState = ui.hooks.useState
+local For = require("ascii-ui.components.for")
+
+--- @type ascii-ui.FunctionalComponent
+local function App()
+	local items, dispatch = ui.hooks.useReducer(function(state, action)
+		if action.type == "add" then
+			return vim.list_extend(state, { "this is " .. #state + 1 })
+		end
+	end, { "this is 1", "this is 2" })
+
+	return Layout(
+		For({
+			items = items,
+			transform = function(item)
+				return { content = item }
+			end,
+			component = Paragraph,
+		}),
+
+		Button({
+			label = "Add more",
+			on_press = function()
+				dispatch({ type = "add" })
+			end,
+		})
+	)
+end
+
+ui.mount(App())
