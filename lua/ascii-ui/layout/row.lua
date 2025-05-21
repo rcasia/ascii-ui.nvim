c9911e599c893a0af677e2a56c200eeb99f8b35f
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-21 21:23:39 +0200

    feat(row): renders row with components having several lines in a row
    #18

--- lua/ascii-ui/layout/row.lua
+++ lua/ascii-ui/layout/row.lua
@@ -1,7 +1,25 @@
 local BufferLine = require("ascii-ui.buffer.bufferline")
 local Element = require("ascii-ui.buffer.element")
+local Layout = require("ascii-ui.layout")
 local createComponent = require("ascii-ui.components.functional-component")
 local logger = require("ascii-ui.logger")
+
+--- @param bufferlines ascii-ui.BufferLine[]
+--- @param other_bufferlines ascii-ui.BufferLine[]
+--- @return ascii-ui.BufferLine[]
+local function merge_bufferlines(bufferlines, other_bufferlines)
+	local max_index = math.max(#bufferlines, #other_bufferlines)
+
+	local merged_bufferlines = {}
+	for i = 1, max_index, 1 do
+		local left_bufferline = bufferlines[i] or BufferLine:new()
+		local right_bufferline = other_bufferlines[i] or BufferLine:new()
+		merged_bufferlines[i] = left_bufferline:append(right_bufferline, Element:new(" "))
+	end
+
+	logger.debug("merge_bufferlines: %s", vim.inspect(merged_bufferlines))
+	return merged_bufferlines
+end
 
 --- @param ... fun(): ascii-ui.BufferLine[]
 --- @return fun(): ascii-ui.BufferLine[]
@@ -17,10 +35,17 @@ local function Row(...)
 
 	return function()
 		local bufferline = BufferLine:new()
-		for i, component_closure in ipairs(component_closures) do
-			bufferline:append(component_closure()[1], Element:new(" "))
-		end
-		return { bufferline }
+
+		return vim.iter(component_closures)
+			:map(function(component_closure)
+				return Layout(component_closure)
+			end)
+			:map(function(layout_closure)
+				return layout_closure()
+			end)
+			:fold({}, function(acc, curr)
+				return merge_bufferlines(acc, curr)
+			end)
 	end
 end
 
--- tests/unit/layout/row_spec.lua
+++ tests/unit/layout/row_spec.lua
@@ -1,22 +1,37 @@
 pcall(require, "luacov")
 
-local DummyComponent = require("tests.util.dummy_functional_component")
+local Element = require("ascii-ui.buffer.element")
+local ui = require("ascii-ui")
 local eq = assert.are.same
 
 local Buffer = require("ascii-ui.buffer")
 local Row = require("ascii-ui.layout.row")
 
 describe("Row", function()
+	local DummyComponent = ui.createComponent("DummyComponent", function(props)
+		props = props or {}
+
+		return function()
+			return {
+				Element:new(props.content):wrap(),
+				Element:new(props.content):wrap(),
+				Element:new(props.content):wrap(),
+			}
+		end
+	end, { content = "string" })
+
 	it("should render a components in a row", function()
 		local row = Row(
 			--
-			DummyComponent(),
-			DummyComponent(),
-			DummyComponent()
+			DummyComponent({ content = "component 1" }),
+			DummyComponent({ content = "component 2" }),
+			DummyComponent({ content = "component 3" })
 		)
 
 		eq({
-			"dummy_render dummy_render dummy_render",
+			"component 1 component 2 component 3",
+			"component 1 component 2 component 3",
+			"component 1 component 2 component 3",
 		}, Buffer:new(unpack(row())):to_lines())
 	end)
 end)
