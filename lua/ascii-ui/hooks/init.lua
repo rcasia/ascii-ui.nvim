7f501fdc1b71a5c1204cbe8449beb1afd96339ba
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-24 18:53:20 +0200

    feat(hooks): expose api for useEffect and useFunctionRegistry

--- lua/ascii-ui/hooks/init.lua
+++ lua/ascii-ui/hooks/init.lua
@@ -2,6 +2,8 @@
 local Hooks = {
 	useState = require("ascii-ui.hooks.use_state"),
 	useReducer = require("ascii-ui.hooks.use_reducer"),
+	useEffect = require("ascii-ui.hooks.use_effect"),
+	useFunctionRegistry = require("ascii-ui.hooks.use_function_registry"),
 }
 
 return Hooks
