52af8e7b05039e4ec698cf0d6e8e3a3088b4c622
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-24 18:49:11 +0200

    feat(hooks): create useFunctionRegistry hook

---
+++ lua/ascii-ui/hooks/use_function_registry.lua
@@ -0,0 +1,11 @@
+--- @param fn function
+--- @return string reference
+local useFunctionRegistry = function(fn)
+	local key = tostring(fn)
+	_G.ascii_ui_function_registry = _G.ascii_ui_function_registry or {}
+	-- vim.g.ascii_ui_function_registry[fn] = vim.g.ascii_ui_function_registry[fn] or {}
+	_G.ascii_ui_function_registry[key] = fn
+	return key
+end
+
+return useFunctionRegistry
