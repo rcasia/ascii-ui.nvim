9edccd1f1334fd88ef6bbced2ca7e80e82667459
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-25 16:44:21 +0200

    doc: generate vimdocs for all files in lua folder

--- scripts/gendocs.lua
+++ scripts/gendocs.lua
@@ -1,14 +1,25 @@
 print("gendocs.lua running...")
 
+local mini_doc_path = "./.deps/mini.doc"
+
 -- clone the mini.doc repository if it doesn't exist
-vim.system({ "git", "clone", "git@github.com:echasnovski/mini.doc.git", "./deps/mini.doc" })
+vim.system({ "git", "clone", "git@github.com:echasnovski/mini.doc.git", mini_doc_path })
 
 -- Añade el path correcto al package.path
-local doc_path = "./deps/mini.doc/lua/?.lua;./deps/mini.doc/lua/?/init.lua"
+local doc_path = ("%s/lua/?.lua;%s/lua/?/init.lua"):format(mini_doc_path, mini_doc_path)
 package.path = doc_path .. ";" .. package.path
+
+-- take all the lua files in project
+local project_files = {}
+local handle = assert(io.popen("find ./lua -type f -name '*.lua'"))
+
+print("Collecting Lua files from the project...")
+for file in handle:lines() do
+	project_files[#project_files + 1] = file
+	print("Found file: " .. file)
+end
+handle:close()
 
 local mini = require("mini.doc")
 mini.setup()
-mini.generate({
-	"lua/ascii-ui/init.lua",
-})
+mini.generate(project_files)
