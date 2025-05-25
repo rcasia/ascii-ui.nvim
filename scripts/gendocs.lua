6919b442eea6ad2483d16ec0b0f71124a5290222
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-25 17:02:25 +0200

    doc: exclude lib files from vimdocs

--- scripts/gendocs.lua
+++ scripts/gendocs.lua
@@ -1,6 +1,18 @@
 print("gendocs.lua running...")
 
 local mini_doc_path = "./.deps/mini.doc"
+local EXCLUDED_FILES = {
+	".-/lib/.-",
+}
+
+local function matches_any(path, patterns)
+	for _, pattern in ipairs(patterns) do
+		if path:match(pattern) then
+			return true
+		end
+	end
+	return false
+end
 
 -- clone the mini.doc repository if it doesn't exist
 vim.system({ "git", "clone", "git@github.com:echasnovski/mini.doc.git", mini_doc_path })
@@ -10,14 +22,19 @@ local doc_path = ("%s/lua/?.lua;%s/lua/?/init.lua"):format(mini_doc_path, mini_d
 package.path = doc_path .. ";" .. package.path
 
 -- take all the lua files in project
-local project_files = {}
 local handle = assert(io.popen("find ./lua -type f -name '*.lua'"))
 
 print("Collecting Lua files from the project...")
-for file in handle:lines() do
-	project_files[#project_files + 1] = file
-	print("Found file: " .. file)
-end
+local project_files = vim.iter(handle:lines())
+	:filter(function(file)
+		return not matches_any(file, EXCLUDED_FILES)
+	end)
+	:map(function(file)
+		print("Found file: " .. file)
+		return file
+	end)
+	:totable()
+
 handle:close()
 
 local mini = require("mini.doc")
