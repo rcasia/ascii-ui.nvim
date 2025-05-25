edf81b77b5c59269d9953d5ab515f473ea486204
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-25 12:12:08 +0200

    test: fetch returns headers and status

--- tests/e2e/fetch_spec.lua
+++ tests/e2e/fetch_spec.lua
@@ -2,15 +2,46 @@ pcall(require, "luacov")
 ---@module "luassert"
 
 local eq = assert.are.same
-local it = it
+
+--- @param stdout string
+--- @return { status: string, headers: table, body: table }
+local function response(stdout)
+	-- first line is status line
+	local lines = vim.split(stdout, "\n", { plain = true })
+
+	-- parse headers
+	-- the headers are separated by an empty line
+
+	local headers = {}
+	for i = 2, #lines do
+		local line = lines[i]
+		if line == "" then
+			break
+		end
+		local key, value = line:match("^(.-):%s*(.*)$")
+		if key and value then
+			headers[key:lower()] = value
+		end
+	end
+
+	-- parse body
+	-- find json body between two empty lines
+	local body = vim.split(stdout, "\r\n\r\n", { plain = true })[2]
+
+	return {
+		status = vim.trim(lines[1]),
+		headers = headers,
+		body = vim.json.decode(body),
+	}
+end
 
 --- @param url string
 local function fetch(url)
-	local result = vim.system({ "curl", "-s", url }):wait()
+	local result = vim.system({ "curl", "-s", "-i", url }):wait()
 	if result.code ~= 0 then
 		return nil
 	end
-	return vim.json.decode(result.stdout)
+	return response(result.stdout)
 end
 
 describe("fetch", function()
@@ -20,6 +51,8 @@ describe("fetch", function()
 
 		assert(response_body, "Response body should not be nil")
 
-		eq(url, response_body.url)
+		eq(url, response_body.body.url)
+		eq("HTTP/2 200", response_body.status)
+		eq("table", type(response_body.headers))
 	end)
 end)
