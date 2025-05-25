09286516e2ba595bc35f65d9798ce864163a4ca5
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-25 17:03:15 +0200

    refactor: translate to english

--- scripts/gendocs.lua
+++ scripts/gendocs.lua
@@ -17,7 +17,7 @@ end
 -- clone the mini.doc repository if it doesn't exist
 vim.system({ "git", "clone", "git@github.com:echasnovski/mini.doc.git", mini_doc_path })
 
--- Añade el path correcto al package.path
+-- add mini.doc to package.path
 local doc_path = ("%s/lua/?.lua;%s/lua/?/init.lua"):format(mini_doc_path, mini_doc_path)
 package.path = doc_path .. ";" .. package.path
 
