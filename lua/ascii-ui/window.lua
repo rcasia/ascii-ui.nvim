f6a15f8f5d4e1c6e8f2c25b1e979aba62126ead4
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-27 18:06:33 +0200

    refactor: move out left drag keymap

--- lua/ascii-ui/window.lua
+++ lua/ascii-ui/window.lua
@@ -73,35 +73,6 @@ function Window:open()
 
 	initialize_window_keymaps(self)
 
-	--- @type ascii-ui.Position
-	local mouse_win_relative_position
-	vim.keymap.set("n", "<LeftMouse>", function()
-		local mouse_pos = vim.fn.getmousepos()
-		mouse_win_relative_position = {
-			line = mouse_pos.screenrow - self:position().line,
-			col = mouse_pos.screencol - self:position().col,
-		}
-		logger.debug("Left mouse key pressed. mouse cursor position: %s", vim.inspect(mouse_pos))
-		logger.debug("window position: %s", vim.inspect(self:position()))
-	end, { buffer = self.bufnr, noremap = true, silent = true })
-
-	vim.keymap.set("n", "<LeftDrag>", function()
-		local mouse_pos = vim.fn.getmousepos()
-		local window_pos = self:position()
-
-		local movement_vector = {
-			line = mouse_pos.screenrow - window_pos.line,
-			col = mouse_pos.screencol - window_pos.col,
-		}
-
-		-- consider the offset
-		local new_position = {
-			line = window_pos.line - mouse_win_relative_position.line + movement_vector.line,
-			col = window_pos.col - mouse_win_relative_position.col + movement_vector.col,
-		}
-
-		self:move_to(new_position)
-	end, { buffer = self.bufnr, noremap = true, silent = true })
 end
 
 --- Get the position of the window
--- lua/ascii-ui/window/keymaps.lua
+++ lua/ascii-ui/window/keymaps.lua
@@ -20,7 +20,40 @@ local on_select = function(window)
 	end, { buffer = window.bufnr, noremap = true, silent = true })
 end
 
+local on_left_drag = function(window)
+	--- @type ascii-ui.Position
+	local mouse_win_relative_position
+	vim.keymap.set("n", "<LeftMouse>", function()
+		local mouse_pos = vim.fn.getmousepos()
+		mouse_win_relative_position = {
+			line = mouse_pos.screenrow - window:position().line,
+			col = mouse_pos.screencol - window:position().col,
+		}
+		logger.debug("Left mouse key pressed. mouse cursor position: %s", vim.inspect(mouse_pos))
+		logger.debug("window position: %s", vim.inspect(window:position()))
+	end, { buffer = window.bufnr, noremap = true, silent = true })
+
+	vim.keymap.set("n", "<LeftDrag>", function()
+		local mouse_pos = vim.fn.getmousepos()
+		local window_pos = window:position()
+
+		local movement_vector = {
+			line = mouse_pos.screenrow - window_pos.line,
+			col = mouse_pos.screencol - window_pos.col,
+		}
+
+		-- consider the offset
+		local new_position = {
+			line = window_pos.line - mouse_win_relative_position.line + movement_vector.line,
+			col = window_pos.col - mouse_win_relative_position.col + movement_vector.col,
+		}
+
+		window:move_to(new_position)
+	end, { buffer = window.bufnr, noremap = true, silent = true })
+end
+
 return function(window)
 	on_quit(window)
 	on_select(window)
+	on_left_drag(window)
 end
