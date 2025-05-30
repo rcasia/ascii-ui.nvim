b3bdeafe359c220a54d5936f442c9a1f2ac58305
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-05-30 18:59:20 +0200

    test: add use_effect_e2e_spec.lua

---
+++ tests/e2e/use_effect_e2e_spec.lua
@@ -0,0 +1,63 @@
+pcall(require, "luacov")
+---@module "luassert"
+
+local eq = assert.are.same
+
+local ui = require("ascii-ui")
+local Paragraph = ui.components.Paragraph
+local Button = ui.components.Button
+local Column = ui.layout.Column
+local Row = ui.layout.Row
+local useState = ui.hooks.useState
+local useEffect = ui.hooks.useEffect
+
+describe("useEffect", function()
+	local useTimer_invocations = 0
+	local useEffect_invocations = 0
+
+	--- @return fun(): string current_time in HH:MM:SS format
+	local function useTimer()
+		local counter, set_counter = useState(0)
+		assert(useTimer_invocations <= 100, "useTimer can only be called once per component render")
+		useTimer_invocations = useTimer_invocations + 1
+		local setInterval = function(callback, interval)
+			return vim.loop.new_timer():start(interval, interval, function()
+				callback()
+			end)
+		end
+
+		local time, set_time = useState(tostring(os.date("%H:%M:%S")))
+
+		useEffect(function()
+			setInterval(function()
+				set_time(tostring(os.date("%H:%M:%S")))
+				set_counter(counter() + 1)
+				useEffect_invocations = useEffect_invocations + 1
+			end, 1000)
+		end)
+
+		return time
+	end
+
+	local App = function()
+		local time = useTimer()
+
+		return function()
+			return Column(
+				Paragraph({ content = "These are some clocks!" }),
+				--
+				Row(Button({ label = time }), Button({ label = time }))
+			)
+		end
+	end
+
+	it("does run just once", function()
+		ui.mount(App())
+		vim.wait(3000, function()
+			return false
+		end)
+
+		eq(useTimer_invocations, 1, "useEffect should only run once per component render")
+		eq(useEffect_invocations, 3, "useEffect should run 3 times: on mount and every second after that")
+	end)
+end)
