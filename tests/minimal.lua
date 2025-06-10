2d4665b2b779594bd5357ae0ecc05637c9ab6b70
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-06-10 07:30:55 +0200

    ci: clone plenary dependency in gh actions

--- tests/minimal.lua
+++ tests/minimal.lua
@@ -15,11 +15,18 @@ end
 -- if in github actions, add a clone of plenary to the package path
 if RUNNING_ON_ACTIONS then
 	local plenary_path = vim.fn.expand("$GITHUB_WORKSPACE/../plenary.nvim")
-	if vim.fn.isdirectory(plenary_path) == 1 then
-		package.path = table.concat({
-			plenary_path .. "/lua/?.lua",
-			plenary_path .. "/lua/?/init.lua",
-			package.path,
-		}, ";")
+	if vim.fn.isdirectory(plenary_path) == 0 then
+		vim.fn.system({
+			"git",
+			"clone",
+			"https://github.com/nvim-lua/plenary.nvim",
+			plenary_path,
+		})
 	end
+
+	package.path = table.concat({
+		plenary_path .. "/lua/?.lua",
+		plenary_path .. "/lua/?/init.lua",
+		package.path,
+	}, ";")
 end
