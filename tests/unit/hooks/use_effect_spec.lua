5fd2ace588de0151fb4699eb186cfa60eeb2ce4d
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-07-27 12:40:52 +0200

    test(useEffect): gets executed as much as dependencies change

--- tests/unit/hooks/use_effect_spec.lua
+++ tests/unit/hooks/use_effect_spec.lua
@@ -33,4 +33,38 @@ describe("useEffect", function()
 
 		eq(log, { "useEffect called" })
 	end)
+
+	it("gets executed as much as dependencies change", function()
+		local log = {}
+		local use_state_log = {}
+		local value, set_value
+		local another_value, set_another_value
+		local Component = ui.createComponent("C", function()
+			return function()
+				value, set_value = ui.hooks.useState(0)
+				another_value, set_another_value = ui.hooks.useState(0)
+				use_state_log[#use_state_log + 1] = "useState called with value: " .. tostring(value)
+				useEffect(function()
+					log[#log + 1] = "useEffect called"
+				end, { value })
+				return { Element:new({ content = tostring(value) .. tostring(another_value) }):wrap() }
+			end
+		end)
+
+		ui.mount(Component)
+
+		set_value(1)
+		set_value(2)
+		set_value(3)
+		set_another_value(1)
+		set_another_value(2)
+		set_another_value(3)
+		set_another_value(4)
+		set_another_value(5)
+		vim.wait(3000, function()
+			return false
+		end)
+
+		eq(#log, 3 + 1)
+	end)
 end)
