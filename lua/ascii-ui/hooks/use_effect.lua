f41f825e263f5bb565eaed5f281dd25346f56beb
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-10-04 18:10:41 +0200

    doc: update useEffect docstring

--- lua/ascii-ui/hooks/use_effect.lua
+++ lua/ascii-ui/hooks/use_effect.lua
@@ -5,32 +5,21 @@ local fiber = require("ascii-ui.fiber")
 ---
 --- Runs a side-effect function after component render and when specified observed values change, inside an ascii-ui component.
 ---
---- Example (runs effect on every render):
---- ```
---- local function MyComponent()
----   local count, setCount = ui.hooks.useState(0)
----   ui.hooks.useEffect(function()
----     print("Rendered!")
----   end)
----   -- component render logic ...
---- end
---- ```
----
 --- Example (runs effect only when count changes):
 --- ```
---- local function MyComponent()
+--- local MyComponent = ui.createComponent("MyComponent", function()
 ---   local count, setCount = ui.hooks.useState(0)
 ---   ui.hooks.useEffect(function()
----     print("Count changed to", count())
+---     print("Count changed to" .. count)
 ---   end, { count })
 ---   -- component render logic ...
---- end
+--- end)
 --- ```
 ---
 --- @param fn function The callback to run as a side effect.
---- @param observed_values? any[] Optional table of state getter functions to observe. If provided, the effect re-runs only when any observed value changes.
-local useEffect = function(fn, observed_values)
-	return fiber._useEffect(fn, observed_values)
+--- @param dependencies? any[] Optional table of state getter functions to observe. If provided, the effect re-runs only when any observed value changes.
+local useEffect = function(fn, dependencies)
+	return fiber._useEffect(fn, dependencies)
 end
 
 return useEffect
