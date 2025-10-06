097d56e13955d023ff6671a836517914527d4522
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-10-07 00:08:09 +0200

    refactor(component): remove unnecesary closure for Slider component

--- lua/ascii-ui/components/slider.lua
+++ lua/ascii-ui/components/slider.lua
@@ -13,44 +13,42 @@ local function Slider(props)
 	props.value = props.value or 0
 	props.title = props.title or ""
 
-	return function()
-		local cc = config.characters
-		local value, setValue = useState(props.value or 0)
+	local cc = config.characters
+	local value, setValue = useState(props.value or 0)
 
-		useEffect(function()
-			if props.on_change then
-				props.on_change(value)
-			end
-		end, { value })
+	useEffect(function()
+		if props.on_change then
+			props.on_change(value)
+		end
+	end, { value })
 
-		local interactions = {
-			[interaction_type.CURSOR_MOVE_RIGHT] = function()
-				setValue(function(v)
-					return math.min(v + 10, 100)
-				end)
-			end,
-			[interaction_type.CURSOR_MOVE_LEFT] = function()
-				setValue(function(v)
-					return math.max(v - 10, 0)
-				end)
-			end,
-		}
+	local interactions = {
+		[interaction_type.CURSOR_MOVE_RIGHT] = function()
+			setValue(function(v)
+				return math.min(v + 10, 100)
+			end)
+		end,
+		[interaction_type.CURSOR_MOVE_LEFT] = function()
+			setValue(function(v)
+				return math.max(v - 10, 0)
+			end)
+		end,
+	}
 
-		local width = 10
-		local knob_position = math.floor(width * value / 100)
+	local width = 10
+	local knob_position = math.floor(width * value / 100)
 
-		return {
-			props.title ~= "" and Segment:new(props.title):wrap(),
-			Bufferline.new(
-				Segment:new({
-					content = cc.horizontal:rep(knob_position),
-				}),
-				Segment:new({ content = cc.thumb, interactions = interactions }),
-				Segment:new({ content = cc.horizontal:rep(width - knob_position) }),
-				Segment:new({ content = (" %d%%"):format(value) })
-			),
-		}
-	end
+	return {
+		props.title ~= "" and Segment:new(props.title):wrap(),
+		Bufferline.new(
+			Segment:new({
+				content = cc.horizontal:rep(knob_position),
+			}),
+			Segment:new({ content = cc.thumb, interactions = interactions }),
+			Segment:new({ content = cc.horizontal:rep(width - knob_position) }),
+			Segment:new({ content = (" %d%%"):format(value) })
+		),
+	}
 end
 
 return createComponent(
