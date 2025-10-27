71741b6f6fe981ef103869f4ad5f814783265607
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-10-27 20:01:15 +0100

    refactor: make use_effect spec to be use_interval spec

--- tests/e2e/use_interval_e2e_spec.lua
+++ tests/e2e/use_interval_e2e_spec.lua
@@ -7,9 +7,9 @@ local Button = ui.components.Button
 local Column = ui.layout.Column
 local Row = ui.layout.Row
 local useState = ui.hooks.useState
-local useEffect = ui.hooks.useEffect
+local useInterval = ui.hooks.useInterval
 
-describe("useEffect", function()
+describe("useInterval", function()
 	local useTimer_invocations = 0
 	local useEffect_invocations = 0
 
@@ -18,44 +18,33 @@ describe("useEffect", function()
 		local counter, set_counter = useState(0)
 		assert(useTimer_invocations <= 100, "useTimer can only be called once per component render")
 		useTimer_invocations = useTimer_invocations + 1
-		local setInterval = function(callback, interval)
-			return vim.uv.new_timer():start(interval, interval, function()
-				callback()
-			end)
-		end
 
 		local time, set_time = useState(tostring(os.date("%H:%M:%S")))
 
-		useEffect(function()
-			setInterval(function()
-				set_time(tostring(os.date("%H:%M:%S")))
-				set_counter(counter + 1)
-				useEffect_invocations = useEffect_invocations + 1
-			end, 30)
-		end, {})
+		useInterval(function()
+			set_time(tostring(os.date("%H:%M:%S")))
+			set_counter(counter + 1)
+			useEffect_invocations = useEffect_invocations + 1
+		end, 30)
 
 		return time
 	end
 
-	local App = ui.createComponent("App", function()
-		local time = useTimer()
-		return Column(
-			Paragraph({ content = "These are some clocks!" }),
-			--
-			Row(Button({ label = time }), Button({ label = time }))
-		)
-	end)
-
-	-- FIXME: when there is useEffect
 	it("does run just once", function()
+		local App = ui.createComponent("App", function()
+			local time = useTimer()
+			return Column(
+				Paragraph({ content = "These are some clocks!" }),
+				--
+				Row(Button({ label = time }), Button({ label = time }))
+			)
+		end)
+
 		ui.mount(App)
 		vim.wait(100, function()
 			return false
 		end)
 
-		-- FIXME: it should be 1?
-		-- but it is 11
-		-- eq(1, useTimer_invocations, "useEffect should only run once per component render")
 		assert(
 			useEffect_invocations >= 3,
 			"useEffect should run at least 3 times after 100ms. Ran just " .. useEffect_invocations
