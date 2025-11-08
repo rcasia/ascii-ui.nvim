4f97acbee65b927821df457fff4d6347b29fc4d6
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-11-08 15:25:28 +0100

    refactor: avoid another call to vim to determine window.bufnr

--- lua/ascii-ui/window/on_select.lua
+++ lua/ascii-ui/window/on_select.lua
@@ -4,14 +4,16 @@ local i = require("ascii-ui.interaction_type")
 local logger = require("ascii-ui.logger")
 local user_interations = require("ascii-ui.user_interactions")
 
+--- @param window ascii-ui.Window
 local on_select = function(window)
 	vim.keymap.set("n", config.keymaps.select, function()
 		logger.debug("--------- USER-INTERACTIONS SELECT ---------")
 		logger.debug("Select key pressed, interacting with user interactions")
-		local bufnr = vim.api.nvim_get_current_buf()
 		local position = Cursor.current_position()
 
-		user_interations:instance():interact({ buffer_id = bufnr, position = position, interaction_type = i.SELECT })
+		user_interations
+			:instance()
+			:interact({ buffer_id = window.bufnr, position = position, interaction_type = i.SELECT })
 	end, { buffer = window.bufnr, noremap = true, silent = true })
 end
 
