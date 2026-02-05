540f8eb2ed1a34970663e995419dd7bab1898299
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-02-05 23:47:07 +0100

    feat: let the user create anonymous components

--- lua/ascii-ui/components/create-component.lua
+++ lua/ascii-ui/components/create-component.lua
@@ -4,11 +4,6 @@ local is_callable = require("ascii-ui.utils.is_callable")
 local logger = require("ascii-ui.logger")
 local memoize = require("ascii-ui.utils.memoize")
 
---- @alias ComponentClosure fun(): ascii-ui.BufferLine[]
---- @alias ascii-ui.ComponentProp<T> T | fun(): T
---- @alias ascii-ui.ComponentProps table<string, ascii-ui.ComponentProp<any>>
---- @alias ascii-ui.FunctionalComponent<P> fun(props: P): ComponentClosure
-
 --- @alias ascii-ui.PropsType
 ---| "nil"
 ---| "number"
@@ -55,17 +50,33 @@ end
 
 --- @alias ascii-ui.TemplateString string
 
+--- @generic ascii-ui.ComponentClosure, T
+--- @alias ascii-ui.SimpleComponentFunction fun(props: T): ascii-ui.FiberNode[]
+---
+
+-- - @generic P : table<string, any>
+--- @alias ascii-ui.FunctionalComponent<P> fun(props?: P): ascii-ui.FiberNode
+
 --- Crea un componente personalizado y lo registra
 --- @generic ascii-ui.ComponentClosure, T
 --- @param name string Nombre del componente
---- @param functional_component fun(props: T): ascii-ui.FiberNode[] | ascii-ui.TemplateString
+--- @param functional_component fun(props: T): ascii-ui.FiberNode[]
 --- @param types? table<string, ascii-ui.PropsType> Tipos de los props del componente
---- @return fun(props?: T): ascii-ui.ComponentClosure component_closure (El closure que renderiza el componente)
+--- @return ascii-ui.FunctionalComponent
+---
+--- @overload fun(functional_component: ascii-ui.SimpleComponentFunction): ascii-ui.FunctionalComponent
 local function createComponent(name, functional_component, types)
-	types = types or {}
+	local opts = { name = name, functional_component = functional_component, types = types or {} }
+
+	if type(opts.name) == "function" then
+		opts.functional_component = opts.name
+		opts.name = "anonymous"
+		opts.types = types or {}
+	end
+
 	-- Validar que el nombre sea único
-	if Renderer.component_tags[name] then
-		logger.error(("El componente con nombre '%s' ya está registrado."):format(name))
+	if Renderer.component_tags[opts.name] then
+		logger.error(("El componente con nombre '%s' ya está registrado."):format(opts.name))
 	end
 
 	-- Generar la pseudofunción del componente
@@ -74,22 +85,23 @@ local function createComponent(name, functional_component, types)
 		__call = function(_, ...)
 			local closure_id = tostring({})
 			logger.debug("Creating closure for component '%s' with id %s", name, closure_id)
-			local args = { ... }
+			local _args = { ... }
 			local factory, props
 
-			if #args == 1 and type(args[1]) == "table" then
-				props = from_function_prop(args[1], types)
-				validate_props(props, types)
+			if #_args == 1 and type(_args[1]) == "table" then
+				-- props = from_function_prop(_args[1], opts.types)
+				props = _args[1] or {}
+				validate_props(props, opts.types)
 				function factory()
 					-- dentro del workLoop, currentFiber ya está seteado
 					return function()
-						return functional_component(props)
+						return opts.functional_component(props)
 					end
 				end
 			else
 				factory = function()
 					return function()
-						return functional_component(unpack(args))
+						return opts.functional_component(unpack(_args))
 					end
 				end
 			end
@@ -98,8 +110,8 @@ local function createComponent(name, functional_component, types)
 
 			return FiberNode.new({
 				tag = "PLACEMENT",
-				type = name,
-				props = props or args,
+				type = opts.name,
+				props = props or _args,
 				closure = closure,
 			})
 		end,
