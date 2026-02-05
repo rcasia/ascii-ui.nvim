4978fb8f5c56a37ecf2930d157211d93735fbb31
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-02-05 22:42:45 +0100

    feat: drop support for For component

--- lua/ascii-ui/components/init.lua
+++ lua/ascii-ui/components/init.lua
@@ -4,7 +4,6 @@ local Components = {
 	Slider = require("ascii-ui.components.slider"),
 	Paragraph = require("ascii-ui.components.paragraph"),
 	Button = require("ascii-ui.components.button"),
-	For = require("ascii-ui.components.for"),
 }
 
 return Components
